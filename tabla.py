e626c8706b1e6ec0dc94593c288cf636951136e2
Luis Carrera <lecarrera@itcr.ac.cr>
2020-10-07 11:50:10 -0600

    Se ajustan las columnas de la tabla, y se agrega una columna nueva para el usuario.

--- tabla.py
+++ tabla.py
@@ -1,16 +1,18 @@
 def encabezadoNotas(notas, numPregs: int) -> None:
-    notas.set_column('A:A', 10)
-    notas.set_column('B:B', 40)
+    notas.set_column('A:A', 15)
+    notas.set_column('B:B', 35)
+    notas.set_column('C:C', 15)
     notas.write(0, 0, '# de carnet')
     notas.write(0, 1, 'Nombre')
-    notas.write(0, 2, 'Nota')
-    notas.write(0, 3, 'Puntaje')
-    # Las primeras 4 columnas est\'an ocupadas.
+    notas.write(0, 2, 'Usuario')
+    notas.write(0, 3, 'Nota')
+    notas.write(0, 4, 'Puntaje')
+    # Las primeras 5 columnas est\'an ocupadas.
     for i in range(numPregs):
-        notas.write(0, i + 4, 'Preg. %d' % (i + 1))
+        notas.write(0, i + 5, 'Preg. %d' % (i + 1))
 
 
 def notasNull(notas, numPregs: int, irow: int) -> None:
-    # Las primeras 4 columnas est\'an ocupadas.
+    # Las primeras 5 columnas est\'an ocupadas.
     for i in range(numPregs):
-        notas.write(irow, i + 4, 0)
+        notas.write(irow, i + 5, 0)
