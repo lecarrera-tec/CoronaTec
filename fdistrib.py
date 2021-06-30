571632bfcacb60eee6ba32e23fa51985d3b055d2
Luis Carrera <lecarrera@itcr.ac.cr>
2021-06-30 09:00:47 -0600

    Se corrige error en la implementaci\'on de la funci\'on gamma. Se mejora c\'alculo de la funci\'on phi inversa.

--- fdistrib.py
+++ fdistrib.py
@@ -12,15 +12,19 @@ def normal(z, mu, sigma):
 
 def phiInv(p: float) -> float:
     assert(0 <= p and p <= 1)
+    signo = 1
+    if p > 0.5:
+        signo = -1
+        p = 1 - p
     f = lambda x: phi(x) - p
     fp = lambda x: math.exp(-0.5 * x**2) / math.sqrt(2 * math.pi)
-    return fmetodos.newton(f, fp, 0)
+    return signo * fmetodos.newton(f, fp, 0)
 
 
 def gamma(x, alfa, beta):
     if beta > 1:
-        beta = 1
         x = x / beta
+        beta = 1
     f = lambda x: x**(alfa - 1) * math.exp(-x/beta) / ((beta ** alfa) * math.gamma(alfa))
     return fmetodos.integral(f, 0, x, [], 0, 1e-6, 20)
 
